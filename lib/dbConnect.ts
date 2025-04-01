99dc835915efa29c973682e82fe83f4cf0697e32
mguzman20 <mguzman@buk.cl>
2025-04-01 15:59:25 -0300

    Refactor database connection to use MONGODB_URI from environment variables

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 
 const connection: {isConnected?: number } = {}
 
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
 async function dbConnect() {
     if(connection.isConnected) {
         return;
     }
 
-    const db = await mongoose.connect(process.env.MONGO_URI!);
+    const db = await mongoose.connect(MONGODB_URI);
 
     connection.isConnected = db.connections[0].readyState;
 }
