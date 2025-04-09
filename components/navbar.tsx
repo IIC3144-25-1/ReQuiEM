f8daa9be47a887291b6cdb2eaebb5322263d6e9b
mguzman20 <mguzman@buk.cl>
2025-04-09 19:22:56 -0400

    fix: center item

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -148,7 +148,7 @@ const Navbar1 = async ({
               </NavigationMenu>
             </div>
           </div>
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
           {user ? (
                 <div className="text-md font-semibold">
                 Hola {user?.name}! 👋
