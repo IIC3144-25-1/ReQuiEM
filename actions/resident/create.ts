d0c2fb09e29b7e8ac31e59acef83eb97191e46d8
rodrigoogalde <rjogalde@uc.cl>
2025-04-18 19:02:11 -0400

    feat: implement resident management features including create and edit functionality

--- actions/resident/create.ts
+++ actions/resident/create.ts
@@ -1,23 +1,13 @@
 'use server'
 
-import { Resident } from "@/models/Resident";
+import { IResident, Resident } from "@/models/Resident";
 import dbConnect from "@/lib/dbConnect";
-import { z } from "zod";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const residentSchema = z.object({
-  user: z.string(),
-  teachers: z.array(z.string()).optional(),
-  specialty: z.string().optional(),
-  year: z.number().optional(),
-});
 
-export async function createResident(data: z.infer<typeof residentSchema>) {
+export async function createResident(data: IResident) {
   const newResident = new Resident({
     user: data.user,
     teachers: data.teachers || [],
-    specialty: data.specialty,
-    year: data.year,
   });
 
   await dbConnect();
