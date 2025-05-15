7eaf0e9d5b9bc40538adbf855aab4779a580d8c1
rodrigoogalde <rjogalde@uc.cl>
2025-05-15 11:59:10 -0400

    feat: enhance resident and teacher management with user email handling and form updates

--- actions/teacher/getByID.ts
+++ actions/teacher/getByID.ts
@@ -4,16 +4,12 @@ import { Teacher, ITeacher } from '@/models/Teacher'
 import dbConnect from '@/lib/dbConnect'
 import { Types } from 'mongoose'
 
-// Función para obtener un profesor por su ID
 export async function getTeacherByID(id: string): Promise<ITeacher | null> {
   await dbConnect()
 
-  // Validar si el ID es válido
   if (!Types.ObjectId.isValid(id)) {
     throw new Error('ID inválido')
   }
-
-  // Buscar y retornar el profesor con el user populado
   const teacher = await Teacher.findById(id)
     .populate('user')
     .populate('area')
